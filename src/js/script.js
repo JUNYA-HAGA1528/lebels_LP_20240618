221465be492e46e0fa29b29bea7a7d4068b49604
junyahaga <n0812034@yahoo.co.jp>
2024-06-23 20:30:42 +0900

    2024/6/23

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,18 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
 
+
+    //帯でのトップスクロールする
+    document.addEventListener('DOMContentLoaded', function() {
+        var scrollToTopBtn = document.getElementById('scroll-to-top');
+        
+        scrollToTopBtn.addEventListener('click', function(event) {
+            event.preventDefault(); // デフォルトのリンク動作を防ぐ
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // スムーズにスクロール
+            });
+        });
+    });
+
 });
